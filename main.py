47efc4eea478b154261c53c07a9bcd18a00fcb06
Clay McLeod <clay.l.mcleod@gmail.com>
2015-10-05 22:38:19 -0500

    added readme

--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas
-df = pandas.read_csv('./data/players.csv')
-
-# example showing most passing yds from the current dataset
-print df.sort(['passing_yds'], ascending=[0]).head(5)[["name", "week", "year", "home", "away", "passing_yds"]]
